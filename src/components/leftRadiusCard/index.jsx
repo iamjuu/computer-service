4dce3426753ef707f255804f627123bf624b5e6c
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-04-19 20:37:21 +0530

    working

---
+++ src/components/leftRadiusCard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const index = ({className}) => {
+  return (
+    <div className={`w-full  ${className}`  }>
+        
+      
+    </div>
+  )
+}
+
+export default index
