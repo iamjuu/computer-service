358662126bbf2c8a1629ef77135f8e54db0f3322
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-04-19 18:47:18 +0530

    working on navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './components/navbar'
+import Footer from './components/footer'
+function App() {
+
+
+  return (
+<>
+<Navbar/>
+<Footer/>
+</>
+  )
+}
+
+export default App
